82f9ad12d778c09831082aefc6151a0696ea92fd
Afrizal <afrizalbagas.03@gmail.com>
2021-01-27 21:50:13 +0700

    Tambah Asset

--- src/assets/icon/index.js
+++ src/assets/icon/index.js
@@ -1,5 +1,6 @@
 import ICMenu from './menu.png';
 import ICMenuDark from './menu-dark.png';
 import ICNext from './next-icon.png';
+import ICBack from './back-icon.png';
 
-export {ICMenu, ICMenuDark, ICNext};
+export {ICMenu, ICMenuDark, ICNext, ICBack};
