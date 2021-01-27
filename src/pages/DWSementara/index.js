4eb809bc5c8832f5c566f3f9947587c7b1e10df2
Afrizal <afrizalbagas.03@gmail.com>
2021-01-27 21:55:46 +0700

    menambah data static

--- src/pages/DWSementara/index.js
+++ src/pages/DWSementara/index.js
@@ -1,14 +1,34 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
+import {Card} from '../../components';
 
-const DWSementara = () => {
+const DWSementara = ({navigation}) => {
   return (
-    <View>
-      <Text>data warga sementara</Text>
+    <View style={styles.page}>
+      <Card
+        nama="Afrizal Bagas"
+        alamat="Gondang timur dalam 2"
+        onPress={() => navigation.navigate('Detail')}
+      />
+      <Card
+        nama="Tuban"
+        alamat="Gondang timur dalam 3"
+        onPress={() => navigation.navigate('Detail')}
+      />
+      <Card
+        nama="Wahider"
+        alamat="Gondang timur dalam 4"
+        onPress={() => navigation.navigate('Detail')}
+      />
     </View>
   );
 };
 
 export default DWSementara;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  page: {
+    backgroundColor: 'white',
+    flex: 1,
+  },
+});
--- src/pages/DWTetap/index.js
+++ src/pages/DWTetap/index.js
@@ -1,13 +1,25 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import {Card} from '../../components';
 
-const DWTetap = () => {
+const DWTetap = ({navigation}) => {
   return (
     <View style={styles.page}>
-      <Card />
-      <Card />
-      <Card />
+      <Card
+        nama="Afrizal Bagas"
+        alamat="Gondang timur dalam 2"
+        onPress={() => navigation.navigate('Detail')}
+      />
+      <Card
+        nama="Tuban"
+        alamat="Gondang timur dalam 3"
+        onPress={() => navigation.navigate('Detail')}
+      />
+      <Card
+        nama="Wahider"
+        alamat="Gondang timur dalam 4"
+        onPress={() => navigation.navigate('Detail')}
+      />
     </View>
   );
 };
