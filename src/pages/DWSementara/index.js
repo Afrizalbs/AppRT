d197240802e063848ec4832dbd909943ebdd54d8
Afrizal <afrizalbagas.03@gmail.com>
2021-02-07 22:33:18 +0700

    intgrasi dengan firebase

--- src/pages/DWSementara/index.js
+++ src/pages/DWSementara/index.js
@@ -1,25 +1,52 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {StyleSheet, View} from 'react-native';
 import {Card} from '../../components';
+import {Firebase} from '../../config';
 
 const DWSementara = ({navigation}) => {
+  const [dataWarga, setDataWarga] = useState([]);
+  useEffect(() => {
+    Firebase.database()
+      .ref('warga/' + 'sementara/')
+      .on('value', (res) => {
+        if (res.val()) {
+          const oldData = res.val();
+          const data = [];
+          Object.keys(oldData).map((key) => {
+            data.push({
+              id: key,
+              data: oldData[key],
+            });
+          });
+          setDataWarga(data);
+        }
+      });
+    // .then((res) => {
+    //   if (res.val()) {
+    //     const oldData = res.val();
+    //     const data = [];
+    //     Object.keys(oldData).map((key) => {
+    //       data.push({
+    //         id: key,
+    //         data: oldData[key],
+    //       });
+    //     });
+    //     setDataWarga(data);
+    //   }
+    // });
+  }, []);
   return (
     <View style={styles.page}>
-      <Card
-        nama="Afrizal Bagas"
-        alamat="Gondang timur dalam 2"
-        onPress={() => navigation.navigate('Detail')}
-      />
-      <Card
-        nama="Tuban"
-        alamat="Gondang timur dalam 3"
-        onPress={() => navigation.navigate('Detail')}
-      />
-      <Card
-        nama="Wahider"
-        alamat="Gondang timur dalam 4"
-        onPress={() => navigation.navigate('Detail')}
-      />
+      {dataWarga.map((warga) => {
+        return (
+          <Card
+            key={warga.id}
+            nama={warga.data.fullName}
+            alamat={warga.data.addressNow}
+            onPress={() => navigation.navigate('Detail2', warga)}
+          />
+        );
+      })}
     </View>
   );
 };
