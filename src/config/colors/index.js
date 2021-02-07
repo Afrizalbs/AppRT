12fe4dbe3543c658df258d85a3e74860cfe7106a
Afrizal <afrizalbagas.03@gmail.com>
2021-02-07 22:32:07 +0700

    add new color

--- src/config/colors/index.js
+++ src/config/colors/index.js
@@ -1,9 +1,10 @@
 export const colors = {
-  utama: '#D9423D',
+  utama: '#ff793f',
   kedua: '#FFCEB0',
   ketiga: '#FFDADA',
   text: {
     utama: '#2d3436',
     kedua: '#dfe6e9',
+    ketiga: '#84817a',
   },
 };
